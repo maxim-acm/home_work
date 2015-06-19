159b4b0bdd57fd5e2028cfaf8092ebe4f00580dd
maxim-acm <maxim-acm@yandex.ru>
2015-06-19 17:35:13 +0300

    add task5

--- javascript_hw/home/task2/task1.js
+++ javascript_hw/home/task2/task1.js
@@ -1,7 +1,5 @@
-/**
- * Created by Maxim on 09.06.2015.
- */
-var year = +prompt("Какой сейчас год?");
+
+    var year = +prompt("Какой сейчас год?");
 
     if (year === 2015) {
         alert("Вы правы!");
