2ef67f64eb2ec0543c7e34c03039f03fab2c06fd
maxim-acm <maxim-acm@yandex.ru>
2015-06-24 14:32:22 +0300

    add task6

---
+++ javascript_hw/home/task6/task6.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Maxim on 24.06.2015.
+ */
+var strings = ['кришна', 'кришна', 'харе', 'харе', 'харе', 'харе', 'кришна', 'кришна', '8-()', 'кришна', 'харе', 'харе', 'харе', 'харе', '8-()', 'кришна', 'харе', '123', 'wqertr', '123' ];
+
+function unique(arr) {
+
+    for (var i = 0; i<arr.length - 1; i++) {
+
+        for (var n = i + 1; n < arr.length; n++) {
+
+            if (arr[i] == arr[n]) {
+
+                arr.splice(i,1);
+
+                n--;
+                i--;
+
+            }
+
+        }
+
+    }
+
+}
+
+
+console.log( unique(strings) );
+console.log(strings);
