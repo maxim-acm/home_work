1d14b47ccc26109275eadc82cd1f9c8afa52ba5d
maxim-acm <maxim-acm@yandex.ru>
2015-07-02 23:18:18 +0300

    add task1

---
+++ javascript_hw/home/task8/task1.js
@@ -0,0 +1,19 @@
+
+        /**
+ * Created by Maxim on 02.07.2015.
+ */
+
+                var ladder = {
+                step: 0,
+                up: function() { // вверх по лестнице
+                this.step++;
+                return this;
+                },
+                down: function() { // вниз по лестнице
+                this.step--;
+                return this;
+                },
+                showStep: function() { // вывести текущую ступеньку
+                alert( this.step );
+                }
+                };
