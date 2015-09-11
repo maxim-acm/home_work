adb059185c8d59dc49ff878bf2dd0ccfecd21ff4
maxim-acm <maxim-acm@yandex.ru>
2015-09-11 18:48:14 +0300

    medium functionality. animation troubles : ( in foul fire

--- javascript_hw/home/exam/js_5.js
+++ javascript_hw/home/exam/js_5.js
@@ -1,102 +1,195 @@
 /**
  * Created by Maxim on 08.09.2015.
  */
-var buttonStart = document.querySelector('.menu__a');
+
+var timeGunMan = 500;
+var flag = 0;   // 0: user can fire, -1: user lost, 1: user win, 2: user fail
+var fail= 0;
+var timeout_id;
+
+var paranja = document.querySelector('.game__over-paranja'),
+    buttonStart = document.querySelector('.menu__a'),
+    statusDiv = document.querySelector('.game__status'),
+    tGunMan = document.querySelector('.gunman_time'),
+    gunman = document.querySelector('.game__character'),
+    divStart = document.querySelector('.game__start');
+
 buttonStart.onclick = function() {
+    console.log('click to start - startGame');
     startGame();
 };
-var gunman = document.querySelector('.game__character');
-var divStart = document.querySelector('.game__start');
+
 function startGame() {
     var aIntro = document.querySelector('.audio__intro');
     aIntro.play();
+
     divStart.classList.add('display-none');
+
     setTimeout(moveGunMan, 1000);
+
+    console.log('startGame - moveGanMan');
 }
 
-var tGunMan = document.querySelector('.gunman_time');
 function moveGunMan() {
     gunman.classList.add('display-block');
-    tGunMan.innerHTML = timeGunMan/1000;
+    tGunMan.innerHTML = (timeGunMan/1000).toFixed(2);
+
     setTimeout(function() {
         gunman.classList.add('character-move');
         gunman.classList.add('character-going');
     }, 100);
+
     startFire();
 
+    console.log('moveGunMan - startFire');
+
 }
-var timeGunMan = 500;
-var statusDiv = document.querySelector('.game__status');
+
 function startFire() {
     gunman.addEventListener('transitionend', function() {
+        var aWait = document.querySelector('.audio__wait');
+        aWait.play();
+
+
+
         gunman.classList.remove('character-going');
         gunman.classList.add('bg-position-in-wait');
 
-        setTimeout(function() {
+        gunman.addEventListener('click', foulFire);
+        console.log('add event: - foulFire');
 
-            var aFire = document.querySelector('.audio__fire');
-            aFire.play();
-            statusDiv.innerHTML = 'FIRE!';
-            statusDiv.classList.add('display-block');
-            flag = 0;
+        timeout_id = setTimeout(function() {
 
-            gunman.classList.add('bg-position-in-start-to-shoot');
+            if (flag == 0) {
+                var aFire = document.querySelector('.audio__fire');
+                aFire.play();
+
+                statusDiv.innerHTML = 'FIRE!';
+                statusDiv.classList.add('display-block');
+                gunman.classList.add('bg-position-in-start-to-shoot');
+            }
+
+
 
             setTimeout(function () {
                 gunman.classList.add('bg-position-in-prefire');
             }, timeGunMan/2);
 
+            calculateTime(new Date().getTime());
+
             userFire();
 
-            setTimeout(function() {
-                gunManFire();
-
-            }, timeGunMan);
+            setTimeout(gunManFire, timeGunMan);
 
         }, 2000);
     })
 
-
 }
-var flag = -1; // 0: user can fire, -1: user lost, 1: user win, 2: user fail
+
+function calculateTime(t) {
+    var tNow;
+
+    function run() {
+        tNow = new Date().getTime();
+
+        if (flag == 0) {
+            var res = ((tNow - t) / 1000).toFixed(2);
+
+            setTimeout(run, 1);
+
+            var tUser = document.querySelector('.user_time');
+            tUser.innerHTML = res;
+        }
+    }
+    run();
+}
+
+
+
+function foulFire() {
+
+    flag = -1;
+    fail = 1;
+    clearTimeout(timeout_id);
+    gunman.removeEventListener('click', foulFire);
+    var aFoul = document.querySelector('.audio__shot-fall');
+    aFoul.play();
+    setTimeout(function() {
+        runStartWindow();
+        console.log('foulFire - runStartWindow');
+    }, 3000);
+}
 
 var changeFlag = function() {
-
+    console.log('changeFlag - win');
     flag = 1;
+
     userWin();
+
     gunman.removeEventListener('click', changeFlag );
+
+
 
 };
 
 function userFire() {
+    gunman.removeEventListener('click', foulFire);
 
-    if (flag == 0) {
+    if (flag == 0 && fail == 0) {
+
         gunman.addEventListener('click', changeFlag);
-        var aShot = document.querySelector('.audio__shot');
-        aShot.play();
+
+        var aShoot = document.querySelector('.audio__shot');
+        aShoot.play();
+
+        console.log('userFire - changeFlag');
     }
 }
 
 function gunManFire() {
-    if (flag == 0) {
-
+    if (flag == 0 && fail == 0) {
+        console.log('gunManFire - gameOver');
         statusDiv.innerHTML = 'YOU LOST!!';
+
         gunman.classList.add('bg-position-in-fire');
+
         flag = -1;
+
         gameOver();
+
+
     }
 }
-var paranja = document.querySelector('.game__over-paranja');
 
 function gameOver() {
     var aDeath = document.querySelector('.audio__death');
     aDeath.play();
+
     paranja.classList.add('display-block');
+
     setTimeout(function() {
         gunman.classList.add('character-move-with-win');
     },1000);
     setTimeout(runStartWindow, 5000);
 
+    console.log('gameOver - runStartWindow');
+
+}
+
+function userWin() {
+    var aWin = document.querySelector('.audio__win');
+    aWin.play();
+
+    statusDiv.innerHTML = 'YOU WON!';
+
+    gunman.classList.add('character-death');
+
+    setTimeout(function() {
+        gunman.classList.add('character-death-full');
+    }, 400);
+    setTimeout(runStartWindow, 4000);
+
+    console.log('win - runStartWindow');
 }
 
 function runStartWindow() {
@@ -114,16 +207,8 @@ function runStartWindow() {
     gunman.classList.remove('character-death');
     gunman.classList.remove('character-death-full');
 
-}
+    console.log('runStartWindow');
 
-function userWin() {
-    var aWin = document.querySelector('.audio__win');
-    aWin.play();
-
-    statusDiv.innerHTML = 'YOU WON!';
-    gunman.classList.add('character-death');
-    setTimeout(function() {
-        gunman.classList.add('character-death-full');
-    }, 400);
-    setTimeout(runStartWindow, 4000);
+    flag = 0;
+    fail = 0;
 }
